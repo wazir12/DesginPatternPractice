8abbc1c31be6e1f1c699179ede1818233b06e95b
lalit.wazir <wazirlalit96420@gmail.com>
2021-01-03 17:51:59 +0530

    SelectionSort with max element at bottom

--- SortingAlgorithms/SortingAlgorithms/src/main/java/SortingAlgorithms/SelectionSort/SelectionSort.java
+++ SortingAlgorithms/SortingAlgorithms/src/main/java/SortingAlgorithms/SelectionSort/SelectionSort.java
@@ -18,9 +18,34 @@ public class SelectionSort {
         int[] arr = {
             40,30,20,1,17
         };
-        performSelectionSort(arr);
+        performSelectionSortWithMaxElementAtBottom(arr);
     }
-
+   private static void performSelectionSortWithMaxElementAtBottom(int[] arr){
+     for(int i=arr.length-1;i>0;i--){
+          int index_of_max = i;
+          System.out.println("Iteration: "+(arr.length - 1 - i));
+          System.out.println("Current Max: "+arr[index_of_max]);
+                for(int j=i-1; j>=0;j--){
+                    
+                    if(arr[j]>arr[index_of_max]){
+                       
+                        //Finding the index of the min element in every iteration
+                        index_of_max=j;
+                        System.out.println("Current Max: "+arr[index_of_max]+" at iteration: "+i);
+                    }
+                }
+                if(index_of_max!=i){
+                    //Swapping the min element at the end of each iteration,
+                    //if index of min element is not equal to i
+                  int temp = arr[i];
+                  arr[i] = arr[index_of_max];
+                  arr[index_of_max] = temp;
+                 
+                }
+                
+                printArray(arr);
+     }  
+   }
     private static void performSelectionSort(int[] arr) {
         //Total Iteration = N-1
             for(int i=0;i<arr.length-1;i++){
